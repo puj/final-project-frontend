acc8ea270d3d96f63f72e699da995e9e7ba2eb6c
puj <vango85@gmail.com>
2020-03-18 09:30:34 +0100

    Use isAuthenticatedUser
    Use the method you've already created to decide whether the user is logged in or not

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styling/header.css'
+import {isAuthenticatedUser} from 'services/auth'
 
 export const Header = () => {
   return (
     <header>
       <h1 className="site-title"><Link to="/">HAGGLE</Link></h1>
       <div className="user-container">
-        {/* if signed in render <icon> <p>{user.name}</p> */}
-        <Link to="/sign-in" className="sign-in-link">Sign in</Link>
-        <Link to="/register" className="registration-link">Register</Link>
+        {isAuthenticatedUser ?
+          <img src=""></img>
+          :
+          <div>
+          <Link to="/sign-in" className="sign-in-link">Sign in</Link>
+          <Link to="/register" className="registration-link">Register</Link>
+          </div>
+        }
       </div>
     </header>
   )
